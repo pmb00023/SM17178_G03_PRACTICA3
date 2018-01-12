40858f8f197d19b773f81de7d4624d45e6a61a4a
pmb00023 <31918770+pmb00023@users.noreply.github.com>
2018-01-12 21:22:08 +0100

    Terminado registro de valores , falta cambiar los idiomas

--- app/src/main/java/es/ujaen/git/sm17178_g03_practica1/Action.java
+++ app/src/main/java/es/ujaen/git/sm17178_g03_practica1/Action.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -20,18 +21,34 @@ public class Action extends AppCompatActivity {
         editPH=(EditText)findViewById(R.id.editPH);
         editTemperatura=(EditText)findViewById(R.id.editTemperatura);
         buttonEnviar=(Button)findViewById(R.id.buttonEnviar);
+        final String humedad=editHumedad.getText().toString();
+        final String ph=editPH.getText().toString();
+         final TextView txt=(TextView)findViewById(R.id.textView16);
+         final String id=editTemperatura.getText().toString();
 
 
+        final SQlite sQlite=new SQlite(this);
+        sQlite.abrir();
+        txt.setText(sQlite.leer());
 
-        final SQlite sQlite=new SQlite(getApplicationContext());
 
         buttonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final String Humedad=editHumedad.getText().toString();
-                final String PH=editPH.getText().toString();
-                final String Temperatura=editTemperatura.getText().toString();
-                sQlite.crearReigo(PH,Humedad,Temperatura);
+                editHumedad=(EditText)findViewById(R.id.editHumedad);
+                editPH=(EditText)findViewById(R.id.editPH);
+                editTemperatura=(EditText)findViewById(R.id.editTemperatura);
+                buttonEnviar=(Button)findViewById(R.id.buttonEnviar);
+                String humedad=editHumedad.getText().toString();
+                 String ph=editPH.getText().toString();
+                final TextView txt=(TextView)findViewById(R.id.textView16);
+                final String id=editTemperatura.getText().toString();
+                sQlite.abrir();
+                sQlite.anadirTab(ph,humedad);
+
+                txt.setText(sQlite.leer());
+                sQlite.cerrar();
+
                 Toast.makeText(getApplicationContext(),"se han almacenado los datos",Toast.LENGTH_SHORT).show();
 
             }
--- app/src/main/java/es/ujaen/git/sm17178_g03_practica1/SQlite.java
+++ app/src/main/java/es/ujaen/git/sm17178_g03_practica1/SQlite.java
@@ -1,39 +1,74 @@
 package es.ujaen.git.sm17178_g03_practica1;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import static android.provider.BaseColumns._ID;
+
 
 /**
  * Created by pablo on 12/01/2018.
  */
 
 public class SQlite extends SQLiteOpenHelper {
-    private static final String Nombre_base="sQlite.db";
-    private static final int Version=1;
-    private static final String TABLA_RIEGO="CREATE TABLE RIEGO(PH TEXT PRIMARY KEY,HUMEDAD TEXT,PH TEXT)";
 
 
-    public SQlite(Context context) {
 
-        super(context,Nombre_base, null, Version);
-    }
-    public void onCreate(SQLiteDatabase sqLiteDatabase){
-
-        sqLiteDatabase.execSQL(TABLA_RIEGO);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLA_RIEGO);
-        sqLiteDatabase.execSQL(TABLA_RIEGO);
+    public SQlite(Context ctx){
+        super(ctx,"sQl.db",null,1);
 
     }
-    public void crearReigo(String ph,String Humedad,String Temperatura){
-        SQLiteDatabase db=getWritableDatabase();
-        if(db !=null){
-            db.execSQL("INSERT INTO RIEGO VALUES('"+ph+"','"+Humedad+"','"+Temperatura+"')");
-            db.close();
-        }
+    public void onCreate(SQLiteDatabase db){
+        String query ="CREATE TABLE campillo("+_ID+" INTEGER PRIMARY KEY AUTOINCREMENT ,"
+        +"ph TEXT, humedad TEXT)";
+        db.execSQL(query);
     }
+
+    public void onUpgrade(SQLiteDatabase db,int version_ant,int version_nue){
+        db.execSQL("DROP TABLE IF EXISTS campillo");
+        onCreate(db);
+    }
+    public void anadirTab(String ph1,String humedad1){
+        ContentValues valores2= new ContentValues();
+        valores2.put("ph",ph1);
+        valores2.put("humedad",humedad1);
+
+        this.getWritableDatabase().insert("campillo",null,valores2);
+
+    }
+
+    public String leer(){
+        String result="";
+        String columnas[]={_ID,"ph","humedad"};
+        Cursor c = this.getReadableDatabase().query("campillo",columnas,null,null,null,null,null);
+
+        int id,iu,ip;
+
+        id= c.getColumnIndex(_ID);
+        iu= c.getColumnIndex("ph");
+        ip= c.getColumnIndex("humedad");
+        c.moveToLast();
+        result= "Sesión nº:"+c.getString(id)+"Valor del PH:"+c.getString(iu)+"Valor de la humedad:"+c.getString(ip)+"\n";
+
+        return result;
+
+
+    }
+
+
+    public void abrir(){
+     this.getWritableDatabase();
+    }
+
+    public void cerrar(){
+        this.close();
+    }
+
+
+
+
 }
